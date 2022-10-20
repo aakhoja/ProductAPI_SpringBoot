546252f0ce781bd6d8ed3233bbb9d12fe7ccba0b
Aman Khoja <aak171730@utdallas.edu>
2022-10-20 00:53:59 -0500

    Product class added, API Implementation using Spring Boot

---
+++ src/main/java/com/example/demo/product/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.demo.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProduct(){
+        return productRepository.findAll();
+    }
+
+    public void addNewProduct(Product product){
+        productRepository.save(product);
+    }
+}
+
