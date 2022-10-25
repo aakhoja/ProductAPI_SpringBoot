55f30584d0abe12ae8446375a3ab28ab098102a5
Aman Khoja <aak171730@utdallas.edu>
2022-10-24 23:58:12 -0500

    added container for product info
    add 3 text field from product info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AppBar from './components/AppBar'
+import Student from './components/Product'
 function App() {
   return (
     <div className="AppBar">
       <AppBar/>
-    </div>
+      <Student/>
+    </div> 
   );
 }
 
---
+++ src/components/Product.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Container , Paper} from '@mui/material';
+
+export default function Product() {
+    const titleBackground = {padding:'50px 20px', width:600,margin:'20px auto'}
+  return (
+
+    <Container>
+        <Paper elevation={3} style={titleBackground}>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="ProductName" variant="outlined" />
+      <TextField id="filled-basic" label="ProductQuantity" variant="filled" />
+      <TextField id="standard-basic" label="ProductSKU" variant="standard" />
+    </Box>
+    </Paper>
+    </Container>
+  );
+}
